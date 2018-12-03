b8c322c896d940374556cd92e59b5855b2d5edf3
Krystian Korzec <43808468+krystiankorzec@users.noreply.github.com>
2018-12-03 16:37:03 +0100

    Update Airly.R

--- Airly.R
+++ Airly.R
@@ -1,57 +1,70 @@
-setwd("c:/users/kkorze01/desktop/traffic")
 library(tidyverse)
 library(httr)
 library(jsonlite)
-req <- GET("https://airapi.airly.eu/v2/measurements/nearest?lat=50.062006&lng=19.940984&maxDistanceKM=5", 
-           authenticate("user", "pass", type = "basic"),
-           add_headers(auth_appkey = "TneT08fQ2LFRpTYINqsg6AkDKUP4m6TD"))
-req
+apikey <- "08x6sSF4ibdLggBLGrWS6TfpWIdVovPv"
+apikey2 <- "TneT08fQ2LFRpTYINqsg6AkDKUP4m6TD"
+adres1 <-"https://airapi.airly.eu/v2/installations/nearest?lat=52.2326046&lng=20.7803207&maxDistanceKM=35&maxResults=-1"
+adres2 <-"https://airapi.airly.eu/v2/measurements/installation?installationId="
 
-req <-GET(url = adres, query = list(api_key = "TneT08fQ2LFRpTYINqsg6AkDKUP4m6TD"))
-
-apikey <- "TneT08fQ2LFRpTYINqsg6AkDKUP4m6TD"
-adres1 <-"https://airapi.airly.eu/v2/installations/nearest?lat=52.2326046&lng=20.7803207&maxDistanceKM=20&maxResults=-1"
-adres2 <-"https://airapi.airly.eu/v2/measurements/nearest?lat=52.2326046&lng=20.7803207&maxDistanceKM=5"
-req <- GET(paste0(adres1,"&apikey=",apikey))
-req.json <- content(req,'text')
-req.df <- fromJSON(req.json, simplifyDataFrame = TRUE)
-req.df$location$latitude
-
-
-
-
-
-library(mapview)
-library(sp)
-airly.spdf <- SpatialPointsDataFrame(coords = data.frame("Long" = req.df$location$longitude,
-                                                "Lat"=req.df$location$latitude),data = data.frame("Sponsor" = req.df$sponsor$name,
-                                                                                                  "ID"=req.df$id))
-
-airly.spdf@proj4string <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
-
-mapview(airly.spdf)
-get_data = function(url) {
-  busy.get = GET(url =url)
-  return(busy.get)
+response <- GET(paste0(adres1,"&apikey=",apikey))
+sensors <- content(response, 'text') %>% fromJSON()
+ids <- sensors$id
+best_sensors <- list()
+for(i in ids){
+  test <- GET(paste0(adres2,i,"&apikey=",apikey))
+  test_df <- content(test,'text') %>% fromJSON()
+  war <- test_df$history$values[[1]] %>% nrow() 
+  try(if(war == 6){
+    best_sensors<- append(best_sensors,i)
+  })
+  Sys.sleep(time=2)
 }
-# get_data2() is a get_data() function with error handling
-get_data2 <- purrr::possibly(get_data, otherwise = NA)
-apikey <- "TneT08fQ2LFRpTYINqsg6AkDKUP4m6TD"
-ids <- req.df$id
-adres <-"https://airapi.airly.eu/v2/measurements/installation?installationId="
-y<-paste0("START","id_",ids)
-output <- list()
-for(i in 1:length(ids)){
-  print(i)
-  url <-paste0(adres,ids[i],"&apikey=",apikey)
-  x<-get_data2(url)
-  output$wynik <- x
-  names(output) <- y[1:i]
-  Sys.sleep(time = 1.25)
-  } 
-  y<- append(y,paste0(Sys.time(),"id_",ids))
-  
-  
-test.json <- content(output$STARTid_2593,'text')
-test.df <- fromJSON(test.json, simplifyDataFrame = TRUE)
-test.df$history$values
+best_sensors <- unlist(best_sensors)
+sensors_df <- data_frame(ID= sensors$id,
+                         Longitude = sensors$location$longitude,
+                         Latitude = sensors$location$latitude)
+
+sensors_best <- filter(sensors_df, ID %in% best_sensors)
+######################################################################
+##########          Data in tidy format       ########################
+######################################################################
+test <- GET(paste0(adres2,best_sensors[1],"&apikey=",apikey))
+test_df <- content(test,'text') %>% fromJSON()
+pomiary <- matrix(ncol=8, nrow=1)
+colnames(pomiary) <- c("ID","Time",test_df$history$values[[1]][,1])
+pomiary[1,1]<-best_sensors[1]  
+pomiary[1,2]<-test_df$history$fromDateTime[[1]]  
+pomiary[1,3:8]<-test_df$history$values[[1]][,2]
+for(l in 2:24){
+  out <- matrix(ncol=8, nrow=1)
+  colnames(out) <- c("ID","Time",test_df$history$values[[1]][,1])
+  out[1,1]<- best_sensors[1] 
+  out[1,2] <- test_df$history$fromDateTime[[l]]  
+  out[1,3:8]<-test_df$history$values[[l]][,2]
+  pomiary <- rbind(pomiary, out)
+}
+
+k=1
+for(i in best_sensors[-1]){
+  print(paste0("Pozostało: " , length(best_sensors[-1])-k))
+  test <- GET(paste0(adres2,i,"&apikey=",apikey))
+  test_df <- content(test,'text') %>% fromJSON()
+  for(m in 1:24){
+    out <- matrix(ncol=8, nrow=1)
+    colnames(out) <- c("ID","Time",test_df$history$values[[1]][,1])
+    out[1,1]<- i
+    out[1,2] <- test_df$history$fromDateTime[[m]]  
+    if(nrow(test_df$history$values[[m]])< 6){
+      out[1,3:8]<-rep(NA, 6)
+      pomiary <- rbind(pomiary, out)
+      next
+    }
+    out[1,3:8]<-test_df$history$values[[m]][,2]
+    pomiary <- rbind(pomiary, out)
+  }
+  k=k+1
+  Sys.sleep(time=5)
+}
+pomiary <- as_data_frame(pomiary)
+pomiary$ID <- as.numeric(pomiary$ID)
+pomiary <- left_join(pomiary, sensors_best, by="ID")
