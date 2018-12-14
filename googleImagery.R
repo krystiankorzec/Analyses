7aa6c4d7901a60970c34e3b9ef0e0fe48bc5c704
Krystian Korzec <43808468+krystiankorzec@users.noreply.github.com>
2018-12-14 21:54:58 +0100

    Update googleImagery.R

--- googleImagery.R
+++ googleImagery.R
@@ -1,7 +1,7 @@
 #if(!requireNamespace("devtools")) install.packages("devtools")
 #devtools::install_github("dkahle/ggmap", ref = "tidyup", force=TRUE)
 #library(ggmap)
-#ggmap::register_google(key = "AIzaSyD6xl3bO0YTlKWJ8Euj3wtoXf-5TgtlFq0")
+#ggmap::register_google(key = "")
 
 cde<- get_map(location = c(lon = 21.2149435, lat = 52.2463433),
                          color = "color",
